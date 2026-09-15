6f69296e404be9913f3f54156c22c341fed2c384
Victor K <victor.kauan@ufpi.edu.br>
2026-09-15 20:24:19 -0300

    Add Cliente model class (issue #3)

---
+++ Cliente.java
@@ -0,0 +1,56 @@
+public class Cliente {
+  private int id;
+  private String nome;
+  private String cpf;
+  private String telefone;
+  private String endereco;
+
+public Cliente(int id, String nome, String cpf, String telefone, String endereco) {
+  this.id = id;
+  this.nome = nome;
+  this.cpf = cpf;
+  this.telefone = telefone;
+  this.endereco = endereco;
+}
+
+public int getId() {
+  return id;
+}
+
+public String getNome() {
+  return nome;
+}
+
+public void setNome(String nome) {
+  this.nome = nome;
+}
+
+public String getCpf() {
+  return cpf;
+}
+
+public void setCpf(String cpf) {
+  this.cpf = cpf;
+}
+
+public String getTelefone() {
+  return telefone;
+}
+
+public void setTelefone(String telefone) {
+  this.telefone = telefone;
+}
+
+public String getEndereco() {
+  return endereco;
+}
+
+public void setEndereco(String endereco) {
+  this.endereco = endereco;
+}
+
+@Override
+  public String toString() {
+    return id + " - " + nome + " | CPF: " + cpf + " | Tel: " + telefone + " | End: " + endereco;
+  }
+}
