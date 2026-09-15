52d77037ca50bf6fd09f06601e94f8fc7853a8dc
Victor K <victor.kauan@ufpi.edu.br>
2026-09-15 20:26:56 -0300

    Implement client listing in Cliente menu, close issue #3

--- Main.java
+++ Main.java
@@ -1,11 +1,34 @@
 import java.util.Arrays;
+import java.util.List;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Menu mainMenu =  new Menu("Menu Principal", Arrays.asList("Conta", "Cliente", "Operacoes"));
-		System.out.println(mainMenu.getSelection() + "foi selecionada");
-		System.out.println("Fim");
+public static void main(String[] args) {
+	Menu mainMenu = new Menu("Menu Principal", Arrays.asList("Conta", "Cliente", "Operacoes"));
+	int selecionado = mainMenu.getSelection();
+
+	if (selecionado == 2) {
+		Menu clienteMenu = new Menu("Menu Cliente", Arrays.asList("Listar Clientes cadastrados"));
+		int opcaoCliente = clienteMenu.getSelection();
+		if (opcaoCliente == 1) {
+			listarClientes();
+		}
+	} else {
+		System.out.println(selecionado + "foi selecionada");
 	}
 
+	System.out.println("Fim");
+}
+
+private static void listarClientes() {
+	List<Cliente> clientes = ClienteRepository.listar();
+	if (clientes.isEmpty()) {
+		System.out.println("Nenhum cliente cadastrado.");
+		return;
+	}
+	System.out.println("\n=== Clientes cadastrados ===");
+	for (Cliente c : clientes) {
+		System.out.println(c);
+	}
+}
 }
