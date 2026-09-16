7e8b24585199c8d93f04c5750662ae2011f76aad
SoaresDavidson <davi504r35@gmail.com>
2026-09-16 15:52:50 -0300

    fix: merge conflicts

--- ContaAbstract.java
+++ ContaAbstract.java
@@ -1,12 +1,18 @@
+import java.util.ArrayList;
+
 public abstract class ContaAbstract {
+
   protected int contaId;
   protected int clienteId;
   protected float saldo;
+
+  protected ArrayList<String> transacoes;
 
   public ContaAbstract(int contaId, int clienteId, float saldo) {
     this.contaId = contaId;
     this.clienteId = clienteId;
     this.saldo = saldo;
+    this.transacoes = new ArrayList<>();
   }
 
   public float getSaldo() {
@@ -21,9 +27,30 @@ public abstract class ContaAbstract {
     return this.clienteId;
   }
 
-  // métodos que podem mudar se tiver diferentes tipos de conta
   public abstract void realizarSaque(float valor);
+
   public abstract void realizarDeposito(float valor);
 
-  public void transferir(ContaAbstract destino, float valor) { this.realizarSaque(valor); destino.realizarDeposito(valor); }
+  public void transferir(ContaAbstract destino, float valor) {
+    this.realizarSaque(valor);
+    destino.realizarDeposito(valor);
+
+    this.transacoes.add(
+        "TRANSFERÊNCIA ENVIADA: R$ " + valor +
+            " para a conta " + destino.getContaId());
+
+    destino.transacoes.add(
+        "TRANSFERÊNCIA RECEBIDA: R$ " + valor +
+            " da conta " + this.getContaId());
+  }
+
+  public void imprimirTransferencias() {
+    System.out.println("Transferências da conta " + this.contaId + ":");
+
+    for (String transacao : transacoes) {
+      if (transacao.startsWith("TRANSFERÊNCIA")) {
+        System.out.println(transacao);
+      }
+    }
+  }
 }
